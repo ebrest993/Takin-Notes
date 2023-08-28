d688c29fa7e6d13c33d860c047f285f38d6bcd19
ebrest993 <elliott.brest@gmail.com>
2023-08-28 12:37:52 -0500

    installed nodemon

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const formData = require('formdata');
+
+const PORT = 3001;
+
+// need to create a window to be utilized in index.js
+
+const form = express();
+
+form.use(express.static('public'));
+
+form.get('/', (req, res) => res.send('This was the first GET function you wrote'));
+
+// form.get()
+console.log("is this working?")
