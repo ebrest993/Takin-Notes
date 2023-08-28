4de71f606be2e63fac449c6ebb21d6616f56322f
ebrest993 <elliott.brest@gmail.com>
2023-08-28 13:43:19 -0500

    restructuring of files and effective route to /notes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const formData = require('formdata');
 
 const PORT = 3001;
 
@@ -10,8 +9,12 @@ const form = express();
 
 form.use(express.static('public'));
 
-form.get('/', (req, res) => res.send('This was the first GET function you wrote'));
+form.get('/', (req, res) => res.send('/public/index.html'));
 
-form.get('/notes', (req, res) => path.join(__dirname, '/Develop/public/notes.html'));
+form.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+form.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
 console.log("is this working?")
