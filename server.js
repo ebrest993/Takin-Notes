6b111dbc8ddec182685086e719fdf2b7c23f113d
ebrest993 <elliott.brest@gmail.com>
2023-08-29 17:52:30 -0500

    working get and post routes.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
 const dbName = ('./db/db.json');
 
 const PORT = 3001;
@@ -13,34 +14,40 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-    const notes = readFromFile('/db/db.json', 'utf8');
-    const parsedNotes = JSON.parse(notes);
-    console.log(parsedNotes);
+    const notes = readFromFile('db/db.json', 'utf8')
+        .then((notes) => {
+            const parsedNotes = JSON.parse(notes);
+            console.log();
+            res.json(parsedNotes);
+        })
 });
 
-// app.post('/api/notes', (req, 
-//     ) => {
-//     const {title, text} = req.body;
-//     console.info((`${title} ${text} means we've gotten this far`))
-//     const finalNote = `[{ "title":"${title}", "text":"${text}" }]`;
-//     fs.appendFile(dbName, finalNote, function(err) { 
-//         if(err) {
-//             console.log('Error:' + (err))
-//         }
-//     })
-// });
+app.post('/api/notes', (req, res) => {
+    const notes = readFromFile('db/db.json', 'utf8')
+        .then((notes) => {
+            const parsedNotes = JSON.parse(notes);
+            const { title, text } = req.body;
+            console.info((`${title} ${text} means we've gotten this far`))
+            const finalNote = { title, text };
+            parsedNotes.push(finalNote);
+            writeToFile('db/db.json', JSON.stringify(parsedNotes))
+                .then((updatedNotes) => {
+                    res.json(updatedNotes);
+                })        
+        })
+});
 
-app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
